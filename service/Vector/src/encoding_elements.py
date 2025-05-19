32e1f8744f67182eb31b0a4e66c17e589fb0a0bb
UICHEOL_HWANG <icuchoel@gmail.com>
2025-05-19 16:15:46 +0900

    Chore: 자잘한 부분 수정

--- service/Vector/src/encoding_elements.py
+++ service/Vector/src/encoding_elements.py
@@ -23,7 +23,10 @@ class Encoding:
     def encode_image(self, image_url):
         
         try:
-            response = requests.get(image_url)
+            headers = {
+                "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
+            }
+            response = requests.get(image_url, headers=headers)
             response.raise_for_status() # 상태 보고 
             
             image = Image.open(io.BytesIO(response.content))
